cf7c640f903d47c92aeb1931a10a4d4a398e646d
AJ Yaseen <46060468+AJ72311@users.noreply.github.com>
2025-07-13 23:36:10 -0500

    Add user-determined grid dimensions functionality

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 
 This project is a digital replication of the classic toy Etch-a-Sketch. As your cursor moves through the grid, it will leave an "ink" trail like a pen would.
 
+## Live Demo
+
+You can view a live demo of this site here: aj72311.github.io/etch-a-sketch
+
 ## Technologies Used
 
 * `HTML5`
 * `CSS3` (with Flexbox for layout)
 * `Javascript`
+
+## Author
+
+Abdulhameed Yaseen - https://github.com/aj72311
--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const GRID_WIDTH = 650;
 const GRID_HEIGHT = 650;
+let initialGridSize = 16;
 
 const containerDiv = document.querySelector("#grid-container");
-let gridSize = 16;
+const changeDimensionsButton = document.querySelector("#dimensions-btn");
+
+createGridDivs(initialGridSize); // create the initial grid with size 16x16 upon page load
+
+changeDimensionsButton.addEventListener("click", () => {
+    const newGridSize = parseInt(prompt(
+        "Please enter a number N, your new grid will be of size NxN pixels!"
+    ));
+
+    // validate user entry was a number
+    if (Number.isNaN(newGridSize)) {
+        alert("Please input a number!");
+    } else if (newGridSize > 100 || newGridSize < 1 ) {
+        alert("Please enter a number less than 100 and greater than 0!");
+    } else {
+        createGridDivs(newGridSize); // create new grid
+    }
+});
 
 // creates an etch-a-sketch grid of size gridSize-X-gridSize
 function createGridDivs(gridSize) {
+    containerDiv.innerHTML = ""; // clear container 
+
     for (let i = 0; i < gridSize ** 2; i++) {
         const newDiv = document.createElement('div');
 
@@ -25,5 +45,3 @@ function createGridDivs(gridSize) {
         containerDiv.appendChild(newDiv);
     }
 }
-
-createGridDivs(gridSize);
